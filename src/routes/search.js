46a33938dfdb0a6792ccdac25bfedc8d8644156d
Jesper Lindsten <lindstenjesper@gmail.com>
2019-10-09 11:43:17 +0200

    added get for case and case id. not conected to a database

--- src/routes/search.js
+++ src/routes/search.js
@@ -1,9 +1,44 @@
 const express = require('express');
 const router = express.Router();
+//const mysql = require('mysql');
 
 router.get('/', (request, response) => {
-    response.send('hellasdsadoc');
+    console.log('123');
+    response.send('hello');
 });
-
+router.get('/case/', (request, response)=>{
+    console.log('inne i case');
+    let sokning = '';
+    /*con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+        let sql = 'SELECT * FROM Cases ';
+        con.query(sql, function (err, result) {
+          if (err) throw err;
+          console.log("Result: " + result);
+          sokning = result;
+        });
+      });
+    response.send(sokning);*/
+    response.send('case utan id');
+});
+router.get('/case/:id', (request, response)=>{
+    console.log('inne i case id');
+    let sokning = '';
+    let id = request.params.id;
+    console.log(id);
+    /*con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+        let sql = 'SELECT * FROM Cases WHERE ID=?';
+        con.query(sql, [id], function (err, result) {
+          if (err) throw err;
+          console.log("Result: " + result);
+          sokning = result;
+        });
+      });
+    response.send(sokning);*/
+    response.send(id);
+});
 
 module.exports = router;
