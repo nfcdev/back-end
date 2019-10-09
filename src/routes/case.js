b1c5f0b4f1d23ae39b638817a9f05520db5a17e5
Jesper Lindsten <lindstenjesper@gmail.com>
2019-10-09 12:23:55 +0200

    Moved case to it's own file, no longer conected to /search

---
+++ src/routes/case.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+//const mysql = require('mysql');
+
+
+router.get('/', (request, response)=>{
+    console.log('inne i case');
+   // let sokning = '';
+    /*con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+        let sql = 'SELECT * FROM Cases ';
+        con.query(sql, function (err, result) {
+          if (err) throw err;
+          console.log("Result: " + result);
+          sokning = result;
+        });
+      });
+    response.send(sokning);*/
+    response.send('case utan id');
+});
+router.get('/:id', (request, response)=>{
+    console.log('inne i case id');
+    //let sokning = '';
+    let id = request.params.id;
+    console.log(id);
+    /*con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+        let sql = 'SELECT * FROM Cases WHERE ID=?';
+        con.query(sql, [id], function (err, result) {
+          if (err) throw err;
+          console.log("Result: " + result);
+          sokning = result;
+        });
+      });
+    response.send(sokning);*/
+    response.send(id);
+});
+
+module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/search', require('./search'));
-
+router.use('/case', require('./case'));
 
 module.exports = router;
