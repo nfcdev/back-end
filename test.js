126bbc7c41fc873ed5229c60139965134c8a6162
Simon Runefors <simru092@student.liu.se>
2019-10-10 12:09:09 +0200

    updated test

--- test.js
+++ test.js
@@ -1,29 +1,62 @@
-const p1 = new Promise((resolve, reject) => {
+// const p1 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('test1')
+//     }, 3000)
+// });
+
+
+const p6 = new Promise((resolve, reject) => {
     setTimeout(() => {
-        resolve('test1')
+        console.log('hej1');
+
+        resolve()
     }, 3000)
 });
 
 const p2 = new Promise((resolve, reject) => {
     setTimeout(() => {
-        console.log('hej1');
+        reject('fel')
+        console.log('bajsa');
         resolve('test1')
     }, 2000)
 });
 
-console.log('hej2');
+// console.log('hej2');
 
-Promise.all([p1, p2]).then((answer) => {
-    console.log(answer);
-}).catch(e => {
-    console.log(e);
-})
 
-const test = {
+// Promise.all([p1, p2]).then((answer) => {
+//     console.log(answer);
+// }).catch(e => {
+//     console.log(e);
+// })
+
+const test2 = {
     body: {
         vapen: ['asd', 'asd']
     }
 }
+
+
+const test = async () => {
+    try {
+        await p6;
+        await p2;
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+const getdatabasedata = () => {
+    setTimeout(() => {
+        return 5;
+    }, 2000)
+}
+
+
+const svar = getdatabasedata();
+
+console.log(svar);
 
 module.exports = {
     test,
