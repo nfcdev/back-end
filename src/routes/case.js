842717d7086b016b7d56d604cb0b9aa14ddf2e64
Jesper Lindsten <lindstenjesper@gmail.com>
2019-10-10 12:59:24 +0200

    cleanad up code from unnecessary prints etc. added comments

--- src/routes/case.js
+++ src/routes/case.js
@@ -2,31 +2,29 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../../connect');
 
-
+//gets all cases
 router.get('/', (request, response)=>{
-    console.log('inne i case');
     pool.getConnection(function(err, connection) {
         if (err) console.log(err);
         let sql = 'SELECT * FROM `Case`';
         connection.query(sql, (err, result) => {
           connection.release();
           if (err) console.log(err);
-          console.log("Result: " + result);
+          console.log("Data received");
           response.send(result);
         });
       });
 });
+//gets case of a specific ID
 router.get('/:id', (request, response)=>{
-    console.log('inne i case id');
     let id = request.params.id;
-    console.log(id);
     pool.getConnection(function(err, connection) {
         if (err) console.log(err);
         let sql = 'SELECT * FROM `Case` WHERE ID=?';
         connection.query(sql, [id], (err, result) => {
           connection.release();
           if (err) console.log(err);
-          console.log("Result: " + result);
+          console.log("Data received");
           response.send(result);
         });
       });
