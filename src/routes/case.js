b409b65b8c67b4913dd9b41fd4e66f5a073a6a04
Jesper Lindsten <lindstenjesper@gmail.com>
2019-10-10 13:42:02 +0200

    added status updates after errors

--- src/routes/case.js
+++ src/routes/case.js
@@ -5,11 +5,17 @@ const pool = require('../../connect');
 //gets all cases
 router.get('/', (request, response)=>{
     pool.getConnection(function(err, connection) {
-        if (err) console.log(err);
+        if (err){
+          console.log(err);
+          response.status(500).send('Cannot conect to server');
+        }
         let sql = 'SELECT * FROM `Case`';
         connection.query(sql, (err, result) => {
           connection.release();
-          if (err) console.log(err);
+          if (err){
+            console.log(err);
+            response.status(500).send('Bad query');
+          }
           console.log("Data received");
           response.send(result);
         });
@@ -19,11 +25,17 @@ router.get('/', (request, response)=>{
 router.get('/:id', (request, response)=>{
     let id = request.params.id;
     pool.getConnection(function(err, connection) {
-        if (err) console.log(err);
-        let sql = 'SELECT * FROM `Case` WHERE ID=?';
+        if (err){
+           console.log(err);
+           response.status(500).send('Cannot conect to server');
+          }
+        let sql = 'SELECT * FROM `Case` WHERE ID = ?';
         connection.query(sql, [id], (err, result) => {
           connection.release();
-          if (err) console.log(err);
+          if (err){
+             console.log(err);
+             response.status(500).send('Bad query');
+            }
           console.log("Data received");
           response.send(result);
         });
