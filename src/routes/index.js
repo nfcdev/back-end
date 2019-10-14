be41d840ceb238fa15c23f73a4dd499fabbe33be
Benjamin Olsson <benjamin.olsson92@gmail.com>
2019-10-14 13:49:30 +0200

    new issue storage-event

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 router.use('/search', require('./search'));
 router.use('/case', require('./case'));
+router.use('/storage-event', require('./storage-event'));
 
 module.exports = router;
