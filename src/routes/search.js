c81e0fb1ec5a01c472079b788dd6a5302a4df4fe
Mattias Li <xueli729@student.liu.sepython3>
2019-10-15 12:22:33 +0200

    dev started.

--- src/routes/search.js
+++ src/routes/search.js
@@ -5,13 +5,28 @@ const router = express.Router();
 const pool = require('../../connect');
 
 router.get('/', (request, response) => {
+    let reference_number = request.query.reference_number;
+    let material_number = request.query.material_number;
+    let location = request.query.location;
+    let shelf = request.query.shelf;
+    let sql_query = "select * from Article";
+
+    if(reference_number || material_number || location || shelf) sql_query = sql_query + " where ";
+    if(reference_number) sql_query = sql_query + "case in (select id from Case where reference_number = 493064) ";
+
     pool.getConnection(function(err, connection) {
         if (err) console.log(err);
-        connection.query('SELECT * FROM Article', (err,rows) => {
-            connection.release();
-            console.log('Data received from Db:\n');
-            response.send(rows);
-        });
+
+
+            connection.query(sql_query, [reference_number], (err,rows) => {
+                connection.release();
+                console.log('Data received from Db:\n');
+                response.send(rows);
+            });
+
+
+
+
     });
 });
 
