03bca5537f656ab97e64fa1e0589f87de39f78b0
Mattias Li <xueli729@student.liu.sepython3>
2019-10-15 16:14:23 +0200

    all parameters in url query implemented. article info needs working on

--- src/routes/search.js
+++ src/routes/search.js
@@ -14,7 +14,7 @@ router.get('/', (request, response) => {
     let parameters = [];
 
     if(reference_number || material_number || location || shelf) sql_query = sql_query + " where ";
-    else 
+    else sql_query = "select * from StorageRoom inner join StorageMap on StorageRoom.id = StorageMap.storage_room inner join Article on StorageMap.article = Article.id inner join `Case` on Case.id = Article.case";
 
     if(reference_number) {
         sql_query = sql_query + "`case` = (select id from `Case` where reference_number = ?) ";
@@ -30,11 +30,20 @@ router.get('/', (request, response) => {
     } 
 
 
+    // Location är storageroom
     if(location){
         if(has_where_condition) sql_query += "and ";
-        sql_query += "id = (select article from StorageMap where storage_room = (select id from StorageRoom where name = ?)) "
+        sql_query += "id in (select article from StorageMap where storage_room = (select id from StorageRoom where name = ?)) "
         has_where_condition = true;
         parameters.push(location);
+    } 
+
+    // Shelf är placement
+    if(shelf){
+        if(has_where_condition) sql_query += "and ";
+        sql_query += "id in (select article from StorageMap where placement = ?) "
+        has_where_condition = true;
+        parameters.push(shelf);
     } 
 
 
@@ -43,9 +52,9 @@ router.get('/', (request, response) => {
         if (err) console.log(err);
 
             //sql_query = "select * from Article where `case`=6"
-            //sql_query = "select id from `Case` where reference_number = 493064"
-            sql_query = "select * from StorageRoom"
+            //sql_query = "select * from Article where id in (select article from StorageMap where storage_room = (select id from StorageRoom where name='Vapen 1'))"
             console.log(sql_query);
+            console.log(parameters);
             connection.query(sql_query, parameters, (err,rows) => {
                 connection.release();
                 console.log('Data received from Db:\n');
