42f11bd8a5457cda598b27ee9d01828b570e4367
Mattias Li <xueli729@student.liu.sepython3>
2019-10-15 14:54:54 +0200

    dev in progress. array done.

--- src/routes/search.js
+++ src/routes/search.js
@@ -10,15 +10,43 @@ router.get('/', (request, response) => {
     let location = request.query.location;
     let shelf = request.query.shelf;
     let sql_query = "select * from Article";
+    let has_where_condition = false;
+    let parameters = [];
 
     if(reference_number || material_number || location || shelf) sql_query = sql_query + " where ";
-    if(reference_number) sql_query = sql_query + "case in (select id from Case where reference_number = 493064) ";
+    else 
+
+    if(reference_number) {
+        sql_query = sql_query + "`case` = (select id from `Case` where reference_number = ?) ";
+        has_where_condition = true;
+        parameters.push(reference_number);
+    }
+
+    if(material_number){
+        if(has_where_condition) sql_query += "and ";
+        sql_query += "material_number = ? ";
+        has_where_condition = true;
+        parameters.push(material_number);
+    } 
+
+
+    if(location){
+        if(has_where_condition) sql_query += "and ";
+        sql_query += "id = (select article from StorageMap where storage_room = (select id from StorageRoom where name = ?)) "
+        has_where_condition = true;
+        parameters.push(location);
+    } 
+
+
 
     pool.getConnection(function(err, connection) {
         if (err) console.log(err);
 
-
-            connection.query(sql_query, [reference_number], (err,rows) => {
+            //sql_query = "select * from Article where `case`=6"
+            //sql_query = "select id from `Case` where reference_number = 493064"
+            sql_query = "select * from StorageRoom"
+            console.log(sql_query);
+            connection.query(sql_query, parameters, (err,rows) => {
                 connection.release();
                 console.log('Data received from Db:\n');
                 response.send(rows);
