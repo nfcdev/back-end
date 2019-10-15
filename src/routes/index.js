903bee256b00d645b76a9fe3fbc382ac3389007a
rasmuselofsson <rasmus.elofsson1@gmail.com>
2019-10-15 18:46:56 +0200

    added API for branch(get, put, delete, post)

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 router.use('/search', require('./search'));
 router.use('/case', require('./case'));
+router.use('/branch', require('./branch'));
 
 module.exports = router;
