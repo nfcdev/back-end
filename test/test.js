e388ece311c2739ec7343cff04fd7f453074742b
Lojdquist <axel.lojdquist@vizlib.com>
2019-11-05 12:25:38 +0100

    updated test

--- test/test.js
+++ test/test.js
@@ -1,3 +1,4 @@
+/* eslint-disable global-require */
 /* eslint-disable no-undef */
 const { expect } = require('chai');
 const request = require('supertest');
@@ -5,46 +6,44 @@ const pool = require('../connect');
 
 let app;
 
-before(function (done) {
-    app = require('../server');
-    app.on('APP_STARTED', () => {
+before((done) => {
+  app = require('../server');
+  app.on('APP_STARTED', () => {
+    done();
+  });
+});
+
+describe('Test Database connection', () => {
+  it('Application should login to database', (done) => {
+    pool.getConnection((err, resp) => {
+      expect(err).equal(null);
+      done();
+    });
+  });
+});
+
+describe('Testing route cases', () => {
+  it('Should return all cases (100 cases)', (done) => {
+    request(app)
+      .get('/case')
+      .end((err, resp) => {
+        const cases = resp.body.length;
+        expect(err).to.equal(null);
+        expect(cases).to.equal(100);
         done();
-    })
+      });
+  });
+
+
+  it('Should return specific case (id: 5)', (done) => {
+    request(app)
+      .get('/case/5')
+      .end((err, resp) => {
+        const reqCase = resp.body;
+        expect(err).to.equal(null);
+        expect(reqCase.length).to.equal(1);
+        expect(reqCase[0].id).to.equal(5);
+        done();
+      });
+  });
 });
-
-describe('Test Database connection',function() {
-    it('Application should login to database', function(done) {
-        pool.getConnection(function(err, resp) {
-            expect(err).equal(null);
-            done();
-        });
-    });
-});
-
-describe('Testing route cases', function(){
-    
-    it('Should return all cases (100 cases)', function(done) {
-        request(app)
-            .get('/case')
-            .end(function(err, resp){
-            const cases = resp.body.length;
-            expect(err).to.equal(null);
-            expect(cases).to.equal(100);
-            done();
-        });
-    });
-
-
-    it('Should return specific case (id: 5)', function(done) {
-        request(app)
-            .get('/case/5')
-            .end(function(err, resp){
-                const reqCase = resp.body;
-                expect(err).to.equal(null);
-                expect(reqCase.length).to.equal(1);
-                expect(reqCase[0].id).to.equal(5);
-                done();
-            })
-    })
-})
-
