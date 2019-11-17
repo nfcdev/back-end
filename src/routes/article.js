df537f2de536ce000c39b313471121ac9b302379
Benjamin Olsson <benjamin.olsson92@gmail.com>
2019-11-18 00:32:22 +0100

    almost implemented issue

--- src/routes/article.js
+++ src/routes/article.js
@@ -276,4 +276,68 @@ router.get('/branch/:branch_id', (request, response) => {
   });
 });
 
+// Check-out an article from specific storage-room id
+router.post('/check-out', (req, res) => {
+  const checkoutArticle = {
+    material_number: req.body.material_number,
+    comment: req.body.comment,
+    storage_room: req.body.storage_room,
+  };
+  if (!checkoutArticle.material_number || !checkoutArticle.storage_room) {
+    res.status(400).send('Bad request');
+  } else {
+    // eslint-disable-next-line consistent-return
+    let sql1 = 'INSERT INTO StorageEvent (action, timestamp, user, comment, package, shelf, storage_room, branch, article)';
+    sql1 += ' SELECT "checked-out", 55555, 1, ?, Package.package_number, Shelf.shelf_name, StorageRoom.name as "storageroom", Branch.name, Article.id FROM Package, Shelf, StorageRoom, Branch, Article WHERE';
+    sql1 += ' Package.id = (select container from StorageMap where article = (select id from Article where material_number = ?)) AND';
+    sql1 += ' (Shelf.id = (select container from StorageMap where article = (select id from Article where material_number = ?)) OR Shelf.id = (select shelf from Package where id = (select container from StorageMap where article = (select id from Article where material_number = ?)))) AND';
+    sql1 += ' StorageRoom.id = ? AND';
+    sql1 += ' Branch.id = (select branch from StorageRoom where id=?) AND';
+    sql1 += ' Article.material_number = ?';
+    let sql2 = 'delete from StorageMap where article = (select id from Article where material_number = ?)';
+    let sql3 = 'select * from StorageEvent order by id desc limit 1';
+    pool.getConnection((err, connection) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).send('Could not connect to server');
+      }
+      connection.query(sql1, [checkoutArticle.comment, checkoutArticle.material_number, checkoutArticle.material_number, checkoutArticle.material_number, checkoutArticle.storage_room, checkoutArticle.storage_room, checkoutArticle.material_number], (err1, result1) => {
+        connection.release();
+        if (err1) {
+          console.log(err1);
+          return res.status(400).send('Bad query1');
+        }
+      });
+    });
+    pool.getConnection((err, connection) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).send('Could not connect to server');
+      }
+      connection.query(sql2, [checkoutArticle.material_number], (err, result) => {
+        connection.release();
+        if (err) {
+          console.log(err);
+          return res.status(400).send('Bad query2');
+        }
+      });
+    });
+    pool.getConnection((err, connection) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).send('Could not connect to server');
+      }
+      connection.query(sql3, [checkoutArticle.comment, checkoutArticle.material_number, checkoutArticle.material_number, checkoutArticle.material_number, checkoutArticle.storage_room, checkoutArticle.storage_room, checkoutArticle.material_number], (err, result) => {
+        connection.release();
+        if (err) {
+          console.log(err);
+          return res.status(400).send('Bad query3');
+        }
+       res.send(result);
+      });
+    });
+  }
+ });
+ 
+
 module.exports = router;
