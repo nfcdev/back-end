936620777f5d4750b8e2af369402ae31c27f51cf
Benjamin Olsson <benjamin.olsson92@gmail.com>
2019-11-17 03:37:57 +0100

    fixing gets

--- src/routes/case.js
+++ src/routes/case.js
@@ -1,45 +1,47 @@
+/* eslint-disable no-shadow */
 const express = require('express');
+
 const router = express.Router();
 const pool = require('../../connect');
 
-//gets all cases
-router.get('/', (request, response)=>{
-    pool.getConnection(function(err, connection) {
-        if (err){
-          console.log(err);
-          response.status(500).send('Cannot conect to server');
-        }
-        let sql = 'SELECT * FROM `Case`';
-        connection.query(sql, (err, result) => {
-          connection.release();
-          if (err){
-            console.log(err);
-            response.status(500).send('Bad query');
-          }
-          console.log("Data received");
-          response.send(result);
-        });
-      });
+// gets all cases
+router.get('/', (request, response) => {
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.log(err);
+      response.status(500).send('Cannot conect to server');
+    }
+    const sql = 'SELECT * FROM `Case`';
+    connection.query(sql, (err, result) => {
+      connection.release();
+      if (err) {
+        console.log(err);
+        response.status(500).send('Bad query');
+      }
+      console.log('Data received');
+      response.send(result);
+    });
+  });
 });
-//gets case of a specific ID
-router.get('/:id', (request, response)=>{
-    let id = request.params.id;
-    pool.getConnection(function(err, connection) {
-        if (err){
-           console.log(err);
-           response.status(500).send('Cannot conect to server');
-          }
-        let sql = 'SELECT * FROM `Case` WHERE ID = ?';
-        connection.query(sql, [id], (err, result) => {
-          connection.release();
-          if (err){
-             console.log(err);
-             response.status(500).send('Bad query');
-            }
-          console.log("Data received");
-          response.send(result);
-        });
-      });
+// gets case of a specific ID
+router.get('/:id', (request, response) => {
+  const { id } = request.params;
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.log(err);
+      response.status(500).send('Cannot conect to server');
+    }
+    const sql = 'SELECT * FROM `Case` WHERE ID = ?';
+    connection.query(sql, [id], (err, result) => {
+      connection.release();
+      if (err) {
+        console.log(err);
+        response.status(500).send('Bad query');
+      }
+      console.log('Data received');
+      response.send(result);
+    });
+  });
 });
 
 module.exports = router;
--- src/routes/search.js
+++ src/routes/search.js
@@ -1,18 +1,20 @@
 const express = require('express');
+
 const router = express.Router();
 
-//Example: Establishing a connection and query to db
+// Example: Establishing a connection and query to db
 const pool = require('../../connect');
 
 router.get('/', (request, response) => {
-    pool.getConnection(function(err, connection) {
-        if (err) console.log(err);
-        connection.query('SELECT * FROM Article', (err,rows) => {
-            connection.release();
-            console.log('Data received from Db:\n');
-            response.send(rows);
-        });
+  // eslint-disable-next-line func-names
+  pool.getConnection((err, connection) => {
+    if (err) console.log(err);
+    connection.query('SELECT * FROM Article', (_err, rows) => {
+      connection.release();
+      console.log('Data received from Db:\n');
+      response.send(rows);
     });
+  });
 });
 
 
