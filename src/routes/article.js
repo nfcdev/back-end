eff0ef099f77835043d11d8c1975605ec319b3a8
Benjamin Olsson <benjamin.olsson92@gmail.com>
2019-11-17 13:50:10 +0100

    started working on issues regarding processing of article

--- src/routes/article.js
+++ src/routes/article.js
@@ -1,29 +1,63 @@
 const express = require('express');
+
 const router = express.Router();
-const pool = require('../util/connect');
 
+// Example: Establishing a connection and query to db
+const pool = require('../../connect');
 
-//gets all articles belonging to a specifik branch
-router.get('/branch/:branch_id', (request, response)=>{
-    const branchid = request.params.branch_id;
-    pool.getConnection(function(err, connection) {
+// Process an article with specific storage-room id
+router.post('/process', (req, res) => {
+  const processArticle = {
+    material_number: req.body.material_number,
+    comment: req.body.comment,
+    storage_room: req.body.storage_room,
+  };
+  if (!processArticle.material_number || !processArticle.storage_room) {
+    res.status(400).send('Bad request');
+  } else {
+    // eslint-disable-next-line consistent-return
+    pool.getConnection((err, connection) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).send('Could not connect to server');
+      }
+      
+      let sql = 'SELECT CASE WHEN EXISTS ((select * from StorageMap where article = (select id from Article where material_number = ?) and container = (select id from Container where current_storage_room = ?))';
+      sql += ' THEN (Delete from Container where current_storage_room = ?)';
+      sql = 'select * from StorageMap where article = (select id from Article where material_number = ?) and container = (select id from Container where current_storage_room = ?';
+      
+      // sql += ' and Select * from StorageMap';
+
+      // eslint-disable-next-line consistent-return
+      connection.query(sql, [processArticle.material_number, processArticle.storage_room, processArticle.storage_room], (err, result) => {
+        connection.release();
         if (err) {
           console.log(err);
-          response.status(500).send('Could not connect to server');
-        } else{
-        let sql = 'SELECT * FROM Article  WHERE id IN (SELECT article FROM StorageMap WHERE container IN (SELECT id FROM Container WHERE current_storage_room IN (SELECT id FROM StorageRoom WHERE branch = ?)))';
-        connection.query(sql, [branchid], (err, result) =>{
-          connection.release();
-          if (err) {
-            console.log(err);
-            response.status(400).send('Bad query');
-          } else {
-          console.log('Data received');
-          response.send(result);
-          }
-        });
-      }
+          return res.status(400).send('Bad query');
+        }
+        console.log('Article has been processed!');
+        console.log(sql);
+        res.send(result);
       });
+    });
+  }
+});
+
+// Create a new storage-event
+router.post('/???', (req, res) => {
+  const storageEvent = {
+    action: req.body.action,
+    timestamp: req.body.timestamp,
+    user: req.body.user,
+    comment: req.body.comment,
+    package: req.body.package,
+    shelf: req.body.shelf,
+    storage_room: req.body.storage_room,
+    article: req.body.article,
+  };
+
+  let sql = 'INSERT INTO StorageEvent (timestamp) VALUES (Select UNIX_TIMESTAMP)';
+
 });
 
 module.exports = router;
