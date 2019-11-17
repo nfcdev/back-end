b1d45a3508837bb9e6d6e6078edf6feca9026371
Gustav Aaro <gustav.aaro@gmail.com>
2019-11-17 14:35:16 +0100

    Fix merge error and some linting

--- src/routes/article.js
+++ src/routes/article.js
@@ -15,7 +15,11 @@ router.post('/', (req, res) => {
     description: req.body.description,
     case: req.body.case,
   };
-  if (!newArticle.material_number || !newArticle.description || !newArticle.case) {
+  if (
+    !newArticle.material_number
+    || !newArticle.description
+    || !newArticle.case
+  ) {
     res.status(400).send('Bad request');
   } else {
     // eslint-disable-next-line consistent-return
@@ -24,8 +28,12 @@ router.post('/', (req, res) => {
         console.log(err);
         return res.status(500).send('Could not connect to server');
       }
-      const sql = 'INSERT INTO Article(material_number, description, `case`) VALUES (?, ?, ?)';
-      const article = [newArticle.material_number, newArticle.description, newArticle.case];
+      const sql =        'INSERT INTO Article(material_number, description, `case`) VALUES (?, ?, ?)';
+      const article = [
+        newArticle.material_number,
+        newArticle.description,
+        newArticle.case,
+      ];
       console.log(sql);
       console.log(article);
       // eslint-disable-next-line consistent-return
@@ -42,21 +50,28 @@ router.post('/', (req, res) => {
   }
 });
 
-
 // Return all articles in DB
 router.get('/', (req, res) => {
   // eslint-disable-next-line func-names
   pool.getConnection((err, connection) => {
-    let sql_query = "select Article.material_number, Case.reference_number, Branch.name as 'branch', StorageRoom.name as 'storage_room',";
-    sql_query += ' CASE WHEN EXISTS (select package_number from Package where id  = (select container from StorageMap where article = Article.id))';
-    sql_query += " THEN (select package_number from Package where id  = (select container from StorageMap where article = Article.id)) ELSE ' - ' END as package,";
-    sql_query += " Shelf.shelf_name as 'shelf', se2.action as 'status',se1.timestamp as 'timestamp', se2.timestamp as 'last modified', Article.description";
-    sql_query += ' FROM Article, `Case`, Branch, StorageRoom, Shelf, StorageEvent as se1, StorageEvent as se2';
+    let sql_query =      "select Article.material_number, Case.reference_number, Branch.name as 'branch', StorageRoom.name as 'storage_room',";
+    sql_query
+      += ' CASE WHEN EXISTS (select package_number from Package where id  = (select container from StorageMap where article = Article.id))';
+    sql_query
+      += " THEN (select package_number from Package where id  = (select container from StorageMap where article = Article.id)) ELSE ' - ' END as package,";
+    sql_query
+      += " Shelf.shelf_name as 'shelf', se2.action as 'status',se1.timestamp as 'timestamp', se2.timestamp as 'last modified', Article.description";
+    sql_query
+      += ' FROM Article, `Case`, Branch, StorageRoom, Shelf, StorageEvent as se1, StorageEvent as se2';
     sql_query += ' WHERE Article.case = Case.id';
-    sql_query += ' and (StorageRoom.id = (select current_storage_room from Container where id = (select container from StorageMap where article = Article.id)))';
-    sql_query += ' and (Shelf.id = (select container from StorageMap where article = Article.id) OR Shelf.id = (select shelf from Package where id = (select container from StorageMap where article = Article.id)))';
-    sql_query += ' AND se1.id = (SELECT id from StorageEvent WHERE article = Article.id ORDER BY timestamp ASC LIMIT 1)';
-    sql_query += ' AND se2.id = (SELECT id from StorageEvent WHERE article = Article.id ORDER BY timestamp DESC LIMIT 1)';
+    sql_query
+      += ' and (StorageRoom.id = (select current_storage_room from Container where id = (select container from StorageMap where article = Article.id)))';
+    sql_query
+      += ' and (Shelf.id = (select container from StorageMap where article = Article.id) OR Shelf.id = (select shelf from Package where id = (select container from StorageMap where article = Article.id)))';
+    sql_query
+      += ' AND se1.id = (SELECT id from StorageEvent WHERE article = Article.id ORDER BY timestamp ASC LIMIT 1)';
+    sql_query
+      += ' AND se2.id = (SELECT id from StorageEvent WHERE article = Article.id ORDER BY timestamp DESC LIMIT 1)';
     sql_query += ' and Branch.id = StorageRoom.branch';
     sql_query += ' ORDER BY Article.material_number asc';
 
@@ -76,16 +91,24 @@ router.get('/:id', (req, res) => {
   pool.getConnection((err, connection) => {
     if (err) console.log(err);
 
-    let sql_query = "select Article.material_number, Case.reference_number, Branch.name as 'branch', StorageRoom.name as 'storage_room',";
-    sql_query += ' CASE WHEN EXISTS (select package_number from Package where id  = (select container from StorageMap where article = Article.id))';
-    sql_query += " THEN (select package_number from Package where id  = (select container from StorageMap where article = Article.id)) ELSE ' - ' END as package,";
-    sql_query += " Shelf.shelf_name as 'shelf', se2.action as 'status',se1.timestamp as 'timestamp', se2.timestamp as 'last modified', Article.description";
-    sql_query += ' FROM Article, `Case`, Branch,  StorageRoom, Shelf, StorageEvent as se1, StorageEvent as se2';
+    let sql_query =      "select Article.material_number, Case.reference_number, Branch.name as 'branch', StorageRoom.name as 'storage_room',";
+    sql_query
+      += ' CASE WHEN EXISTS (select package_number from Package where id  = (select container from StorageMap where article = Article.id))';
+    sql_query
+      += " THEN (select package_number from Package where id  = (select container from StorageMap where article = Article.id)) ELSE ' - ' END as package,";
+    sql_query
+      += " Shelf.shelf_name as 'shelf', se2.action as 'status',se1.timestamp as 'timestamp', se2.timestamp as 'last modified', Article.description";
+    sql_query
+      += ' FROM Article, `Case`, Branch,  StorageRoom, Shelf, StorageEvent as se1, StorageEvent as se2';
     sql_query += ' WHERE Article.case = Case.id';
-    sql_query += ' and (StorageRoom.id = (select current_storage_room from Container where id = (select container from StorageMap where article = Article.id)))';
-    sql_query += ' and (Shelf.id = (select container from StorageMap where article = Article.id) OR Shelf.id = (select shelf from Package where id = (select container from StorageMap where article = Article.id)))';
-    sql_query += ' AND se1.id = (SELECT id from StorageEvent WHERE article = Article.id ORDER BY timestamp ASC LIMIT 1)';
-    sql_query += ' AND se2.id = (SELECT id from StorageEvent WHERE article = Article.id ORDER BY timestamp DESC LIMIT 1)';
+    sql_query
+      += ' and (StorageRoom.id = (select current_storage_room from Container where id = (select container from StorageMap where article = Article.id)))';
+    sql_query
+      += ' and (Shelf.id = (select container from StorageMap where article = Article.id) OR Shelf.id = (select shelf from Package where id = (select container from StorageMap where article = Article.id)))';
+    sql_query
+      += ' AND se1.id = (SELECT id from StorageEvent WHERE article = Article.id ORDER BY timestamp ASC LIMIT 1)';
+    sql_query
+      += ' AND se2.id = (SELECT id from StorageEvent WHERE article = Article.id ORDER BY timestamp DESC LIMIT 1)';
     sql_query += ' AND Article.id = ?';
     sql_query += ' and Branch.id = StorageRoom.branch';
     sql_query += ' ORDER BY Article.material_number asc';
@@ -109,18 +132,26 @@ router.get('/case/:id', (req, res) => {
       console.log(err);
       return res.status(500).send('Could not connect to server');
     }
-    let sql_query = "select distinct Article.material_number, Case.reference_number, Branch.name as 'branch', StorageRoom.name as 'storage_room',";
-    sql_query += ' CASE WHEN EXISTS (select package_number from Package where id  = (select container from StorageMap where article = Article.id))';
-    sql_query += " THEN (select package_number from Package where id  = (select container from StorageMap where article = Article.id)) ELSE ' - ' END as package,";
-    sql_query += " Shelf.shelf_name as 'shelf', se2.action as 'status',se1.timestamp as 'timestamp', se2.timestamp as 'last modified', Article.description";
-    sql_query += ' FROM Article, `Case`, Branch, StorageRoom, Shelf, Package, Container, StorageEvent as se1, StorageEvent as se2';
+    let sql_query =      "select distinct Article.material_number, Case.reference_number, Branch.name as 'branch', StorageRoom.name as 'storage_room',";
+    sql_query
+      += ' CASE WHEN EXISTS (select package_number from Package where id  = (select container from StorageMap where article = Article.id))';
+    sql_query
+      += " THEN (select package_number from Package where id  = (select container from StorageMap where article = Article.id)) ELSE ' - ' END as package,";
+    sql_query
+      += " Shelf.shelf_name as 'shelf', se2.action as 'status',se1.timestamp as 'timestamp', se2.timestamp as 'last modified', Article.description";
+    sql_query
+      += ' FROM Article, `Case`, Branch, StorageRoom, Shelf, Package, Container, StorageEvent as se1, StorageEvent as se2';
     sql_query += ' WHERE Article.case = Case.id';
-    sql_query += ' and (StorageRoom.id = (select current_storage_room from Container where id = (select container from StorageMap where article = Article.id)))';
-    sql_query += ' and (Shelf.id = (select container from StorageMap where article = Article.id) OR Shelf.id = (select shelf from Package where id = (select container from StorageMap where article = Article.id)))';
-    sql_query += ' AND se1.id = (SELECT id from StorageEvent WHERE article = Article.id ORDER BY timestamp ASC LIMIT 1)';
-    sql_query += ' AND se2.id = (SELECT id from StorageEvent WHERE article = Article.id ORDER BY timestamp DESC LIMIT 1)';
+    sql_query
+      += ' and (StorageRoom.id = (select current_storage_room from Container where id = (select container from StorageMap where article = Article.id)))';
+    sql_query
+      += ' and (Shelf.id = (select container from StorageMap where article = Article.id) OR Shelf.id = (select shelf from Package where id = (select container from StorageMap where article = Article.id)))';
+    sql_query
+      += ' AND se1.id = (SELECT id from StorageEvent WHERE article = Article.id ORDER BY timestamp ASC LIMIT 1)';
+    sql_query
+      += ' AND se2.id = (SELECT id from StorageEvent WHERE article = Article.id ORDER BY timestamp DESC LIMIT 1)';
     sql_query += ' and Branch.id = StorageRoom.branch';
-    sql_query += ' and Case.reference_number = ?';
+    sql_query += ' and Case.id = ?';
     sql_query += ' ORDER BY Article.material_number asc';
 
     if (err) console.log(err);
@@ -144,17 +175,25 @@ router.get('/storageroom/:id', (req, res) => {
       console.log(err);
       return res.status(500).send('Could not connect to server');
     }
-    let sql_query = "select distinct Article.material_number, Case.reference_number, Branch.name as 'branch', StorageRoom.name as 'storage_room',";
-    sql_query += ' CASE WHEN EXISTS (select package_number from Package where id  = (select container from StorageMap where article = Article.id))';
-    sql_query += " THEN (select package_number from Package where id  = (select container from StorageMap where article = Article.id)) ELSE ' - ' END as package,";
-    sql_query += " Shelf.shelf_name as 'shelf', se2.action as 'status',se1.timestamp as 'timestamp', se2.timestamp as 'last modified', Article.description";
-    sql_query += ' FROM Article, `Case`, Branch, StorageRoom, Shelf, Package, Container, StorageEvent as se1, StorageEvent as se2';
+    let sql_query =      "select distinct Article.material_number, Case.reference_number, Branch.name as 'branch', StorageRoom.name as 'storage_room',";
+    sql_query
+      += ' CASE WHEN EXISTS (select package_number from Package where id  = (select container from StorageMap where article = Article.id))';
+    sql_query
+      += " THEN (select package_number from Package where id  = (select container from StorageMap where article = Article.id)) ELSE ' - ' END as package,";
+    sql_query
+      += " Shelf.shelf_name as 'shelf', se2.action as 'status',se1.timestamp as 'timestamp', se2.timestamp as 'last modified', Article.description";
+    sql_query
+      += ' FROM Article, `Case`, Branch, StorageRoom, Shelf, Package, Container, StorageEvent as se1, StorageEvent as se2';
     sql_query += ' WHERE Article.case = Case.id';
     sql_query += ' and StorageRoom.id = ?';
-    sql_query += ' and (StorageRoom.id = (select current_storage_room from Container where id = (select container from StorageMap where article = Article.id)))';
-    sql_query += ' and (Shelf.id = (select container from StorageMap where article = Article.id) OR Shelf.id = (select shelf from Package where id = (select container from StorageMap where article = Article.id)))';
-    sql_query += ' AND se1.id = (SELECT id from StorageEvent WHERE article = Article.id ORDER BY timestamp ASC LIMIT 1)';
-    sql_query += ' AND se2.id = (SELECT id from StorageEvent WHERE article = Article.id ORDER BY timestamp DESC LIMIT 1)';
+    sql_query
+      += ' and (StorageRoom.id = (select current_storage_room from Container where id = (select container from StorageMap where article = Article.id)))';
+    sql_query
+      += ' and (Shelf.id = (select container from StorageMap where article = Article.id) OR Shelf.id = (select shelf from Package where id = (select container from StorageMap where article = Article.id)))';
+    sql_query
+      += ' AND se1.id = (SELECT id from StorageEvent WHERE article = Article.id ORDER BY timestamp ASC LIMIT 1)';
+    sql_query
+      += ' AND se2.id = (SELECT id from StorageEvent WHERE article = Article.id ORDER BY timestamp DESC LIMIT 1)';
     sql_query += ' and Branch.id = StorageRoom.branch';
     sql_query += ' ORDER BY Article.material_number asc';
 
@@ -179,19 +218,28 @@ router.get('/package/:id', (req, res) => {
       console.log(err);
       return res.status(500).send('Could not connect to server');
     }
-    let sql_query = "select distinct Article.material_number, Case.reference_number, Branch.name as 'branch', StorageRoom.name as 'storage_room',";
-    sql_query += ' CASE WHEN EXISTS (select package_number from Package where id  = (select container from StorageMap where article = Article.id))';
-    sql_query += " THEN (select package_number from Package where id  = (select container from StorageMap where article = Article.id)) ELSE ' - ' END as package,";
-    sql_query += " Shelf.shelf_name as 'shelf', se2.action as 'status',se1.timestamp as 'timestamp', se2.timestamp as 'last modified', Article.description";
-    sql_query += ' FROM Article, `Case`, Branch, StorageRoom, Shelf, Package, Container, StorageEvent as se1, StorageEvent as se2';
+    let sql_query =      "select distinct Article.material_number, Case.reference_number, Branch.name as 'branch', StorageRoom.name as 'storage_room',";
+    sql_query
+      += ' CASE WHEN EXISTS (select package_number from Package where id  = (select container from StorageMap where article = Article.id))';
+    sql_query
+      += " THEN (select package_number from Package where id  = (select container from StorageMap where article = Article.id)) ELSE ' - ' END as package,";
+    sql_query
+      += " Shelf.shelf_name as 'shelf', se2.action as 'status',se1.timestamp as 'timestamp', se2.timestamp as 'last modified', Article.description";
+    sql_query
+      += ' FROM Article, `Case`, Branch, StorageRoom, Shelf, Package, Container, StorageEvent as se1, StorageEvent as se2';
     sql_query += ' WHERE Article.case = Case.id';
-    sql_query += ' and (StorageRoom.id = (select current_storage_room from Container where id = (select container from StorageMap where article = Article.id)))';
-    sql_query += ' and (Shelf.id = (select container from StorageMap where article = Article.id) OR Shelf.id = (select shelf from Package where id = (select container from StorageMap where article = Article.id)))';
-    sql_query += ' AND se1.id = (SELECT id from StorageEvent WHERE article = Article.id ORDER BY timestamp ASC LIMIT 1)';
-    sql_query += ' AND se2.id = (SELECT id from StorageEvent WHERE article = Article.id ORDER BY timestamp DESC LIMIT 1)';
+    sql_query
+      += ' and (StorageRoom.id = (select current_storage_room from Container where id = (select container from StorageMap where article = Article.id)))';
+    sql_query
+      += ' and (Shelf.id = (select container from StorageMap where article = Article.id) OR Shelf.id = (select shelf from Package where id = (select container from StorageMap where article = Article.id)))';
+    sql_query
+      += ' AND se1.id = (SELECT id from StorageEvent WHERE article = Article.id ORDER BY timestamp ASC LIMIT 1)';
+    sql_query
+      += ' AND se2.id = (SELECT id from StorageEvent WHERE article = Article.id ORDER BY timestamp DESC LIMIT 1)';
     sql_query += ' and Branch.id = StorageRoom.branch';
     sql_query += ' and Package.id = ?';
-    sql_query += ' AND Package.id = (select id from Container where Container.id = (select container from StorageMap where article = Article.id))';
+    sql_query
+      += ' AND Package.id = (select id from Container where Container.id = (select container from StorageMap where article = Article.id))';
     sql_query += ' ORDER BY Article.material_number asc';
 
     if (err) console.log(err);
@@ -213,7 +261,7 @@ router.get('/branch/:branch_id', (request, response) => {
       console.log(err);
       response.status(500).send('Could not connect to server');
     } else {
-      const sql = 'SELECT * FROM Article  WHERE id IN (SELECT article FROM StorageMap WHERE container IN (SELECT id FROM Container WHERE current_storage_room IN (SELECT id FROM StorageRoom WHERE branch = ?)))';
+      const sql =        'SELECT * FROM Article  WHERE id IN (SELECT article FROM StorageMap WHERE container IN (SELECT id FROM Container WHERE current_storage_room IN (SELECT id FROM StorageRoom WHERE branch = ?)))';
       connection.query(sql, [branchid], (err, result) => {
         connection.release();
         if (err) {
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/search', require('./search'));
-<<<<<<< HEAD
-router.use('/article', require('./article'));
-=======
 router.use('/storageroom', require('./storageroom'));
->>>>>>> f3afbd880b52f5d20d7029a9c7e41cf445caa6dc
 router.use('/case', require('./case'));
 router.use('/login', require('./login'));
 router.use('/logout', require('./logout'));
