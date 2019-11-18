f04f4790ccb8ddcac5d2ee1f12252ff43130c487
Benjamin Olsson <benjamin.olsson92@gmail.com>
2019-11-18 04:16:36 +0100

    implemented checkout of article

--- src/routes/article.js
+++ src/routes/article.js
@@ -287,28 +287,49 @@ router.post('/check-out', (req, res) => {
     res.status(400).send('Bad request');
   } else {
     // eslint-disable-next-line consistent-return
+
+
     let sql1 = 'INSERT INTO StorageEvent (action, timestamp, user, comment, package, shelf, storage_room, branch, article)';
-    sql1 += ' SELECT "checked-out", 55555, 1, ?, Package.package_number, Shelf.shelf_name, StorageRoom.name as "storageroom", Branch.name, Article.id FROM Package, Shelf, StorageRoom, Branch, Article WHERE';
-    sql1 += ' Package.id = (select container from StorageMap where article = (select id from Article where material_number = ?)) AND';
+
+    sql1 += ' SELECT "checked_out", 55555, 1, ?,';
+
+    sql1 += ' CASE WHEN EXISTS (select package_number from Package where id  = (select container from StorageMap where article = (select id from Article where material_number = ?)))';
+
+    sql1 += " THEN (select package_number from Package where id  = (select container from StorageMap where article = (select id from Article where material_number = ?))) ELSE ' - ' END as package,";
+
+    sql1 += ' Shelf.shelf_name, StorageRoom.name as "storageroom", Branch.name, Article.id FROM Shelf, StorageRoom, Branch, Article WHERE';
+
     sql1 += ' (Shelf.id = (select container from StorageMap where article = (select id from Article where material_number = ?)) OR Shelf.id = (select shelf from Package where id = (select container from StorageMap where article = (select id from Article where material_number = ?)))) AND';
+
     sql1 += ' StorageRoom.id = ? AND';
+
     sql1 += ' Branch.id = (select branch from StorageRoom where id=?) AND';
+
     sql1 += ' Article.material_number = ?';
+
     let sql2 = 'delete from StorageMap where article = (select id from Article where material_number = ?)';
+
     let sql3 = 'select * from StorageEvent order by id desc limit 1';
+
+    //sql3 = "select material_number from Article where id in (select article from StorageMap) order by material_number asc";
+
     pool.getConnection((err, connection) => {
       if (err) {
         console.log(err);
         return res.status(500).send('Could not connect to server');
       }
-      connection.query(sql1, [checkoutArticle.comment, checkoutArticle.material_number, checkoutArticle.material_number, checkoutArticle.material_number, checkoutArticle.storage_room, checkoutArticle.storage_room, checkoutArticle.material_number], (err1, result1) => {
+      connection.query(sql1, [checkoutArticle.comment, checkoutArticle.material_number, checkoutArticle.material_number, checkoutArticle.material_number, checkoutArticle.material_number, checkoutArticle.storage_room, checkoutArticle.storage_room, checkoutArticle.material_number], (err1, result1) => {
         connection.release();
         if (err1) {
           console.log(err1);
           return res.status(400).send('Bad query1');
         }
+        console.log(sql1);
       });
     });
+
+
+
     pool.getConnection((err, connection) => {
       if (err) {
         console.log(err);
@@ -322,22 +343,27 @@ router.post('/check-out', (req, res) => {
         }
       });
     });
+
+
+
+
     pool.getConnection((err, connection) => {
       if (err) {
         console.log(err);
         return res.status(500).send('Could not connect to server');
       }
-      connection.query(sql3, [checkoutArticle.comment, checkoutArticle.material_number, checkoutArticle.material_number, checkoutArticle.material_number, checkoutArticle.storage_room, checkoutArticle.storage_room, checkoutArticle.material_number], (err, result) => {
+      connection.query(sql3, (err, result3) => {
         connection.release();
         if (err) {
           console.log(err);
           return res.status(400).send('Bad query3');
         }
-       res.send(result);
+        res.send(result3);
       });
     });
+
+
   }
- });
- 
+});
 
 module.exports = router;
